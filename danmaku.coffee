14c56736c466b32fa8ef3ad3c00221a2b84a5890
Yusuke Hideshima <yusuke.hideshima@gamepot.co.jp>
2012-02-24 14:54:10 +0900

    キャラクタが表示されるようになった

--- danmaku.coffee
+++ danmaku.coffee
@@ -9,10 +9,38 @@ class DanmakuGame extends Game
     super 512, 512
     @fps = 60
     # preloadする素材まわりはこのへんに書いておく
+    @preload("./image/chara0.gif")
   
   onload : ->
     label = new Label "東方発火損"
     label.font = "28px 'osaka-mono'"
     label.backgroundColor = "red"
     @rootScene.addChild label
+    enemy = new Enemy(@assets["./image/chara0.gif"])
+    enemy.frame = 0
+    @rootScene.addChild enemy
     return
+
+class BodyBase extends Sprite
+  x : 0
+  y : 0
+  constructor : (width, height, x, y, image) ->
+    super width, height
+    @scaleX = 2
+    @scaleY = 2
+    @x = x
+    @y = y
+    @image = image
+
+class Player extends BodyBase
+  construnctor : (image) ->
+    super 32, 32, 256, 480, image
+
+class Enemy extends BodyBase
+  constructor : (image) ->
+    super 32, 32, 256, 0, image
+
+
+class Bullet extends Sprite
+
+
--- danmaku.js
+++ danmaku.js
@@ -1,4 +1,4 @@
-var DanmakuGame;
+var BodyBase, Bullet, DanmakuGame, Enemy, Player;
 var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
 enchant();
@@ -10,16 +10,81 @@ DanmakuGame = (function() {
   function DanmakuGame() {
     DanmakuGame.__super__.constructor.call(this, 512, 512);
     this.fps = 60;
+    this.preload("./image/chara0.gif");
   }
 
   DanmakuGame.prototype.onload = function() {
-    var label;
+    var enemy, label;
     label = new Label("東方発火損");
     label.font = "28px 'osaka-mono'";
     label.backgroundColor = "red";
     this.rootScene.addChild(label);
+    enemy = new Enemy(this.assets["./image/chara0.gif"]);
+    enemy.frame = 0;
+    this.rootScene.addChild(enemy);
   };
 
   return DanmakuGame;
+
+})();
+
+BodyBase = (function() {
+
+  __extends(BodyBase, Sprite);
+
+  BodyBase.prototype.x = 0;
+
+  BodyBase.prototype.y = 0;
+
+  function BodyBase(width, height, x, y, image) {
+    BodyBase.__super__.constructor.call(this, width, height);
+    this.scaleX = 2;
+    this.scaleY = 2;
+    this.x = x;
+    this.y = y;
+    this.image = image;
+  }
+
+  return BodyBase;
+
+})();
+
+Player = (function() {
+
+  __extends(Player, BodyBase);
+
+  function Player() {
+    Player.__super__.constructor.apply(this, arguments);
+  }
+
+  Player.prototype.construnctor = function(image) {
+    return Player.__super__.construnctor.call(this, 32, 32, 256, 480, image);
+  };
+
+  return Player;
+
+})();
+
+Enemy = (function() {
+
+  __extends(Enemy, BodyBase);
+
+  function Enemy(image) {
+    Enemy.__super__.constructor.call(this, 32, 32, 256, 0, image);
+  }
+
+  return Enemy;
+
+})();
+
+Bullet = (function() {
+
+  __extends(Bullet, Sprite);
+
+  function Bullet() {
+    Bullet.__super__.constructor.apply(this, arguments);
+  }
+
+  return Bullet;
 
 })();
